fb844748350f8e8f06e08bbc34a316ad2befed80
Mahesh <seleniumbymahesh@gmail.com>
2018-08-18 11:44:15 +0530

    Revert "sample2 added"
    This reverts commit 25ac9317874934e3248722a96a4e9feabf9cc23c.

--- project1/src/pack3/Sample.java
+++ project1/src/pack3/Sample.java
@@ -4,7 +4,7 @@ public class Sample {
 
 	public static void main(String[] args) {
 	System.out.println("Hi welcome to Github");
-	System.out.println("By to git hub");
+
 	}
 
 }
--- project1/src/pack3/Sample1.java
+++ project1/src/pack3/Sample1.java
@@ -3,7 +3,7 @@ package pack3;
 public class Sample1 {
 
 	public static void main(String[] args) {
-		System.out.println("Hi");
+		// TODO Auto-generated method stub
 
 	}
 
