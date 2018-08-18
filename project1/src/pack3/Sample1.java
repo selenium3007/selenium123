decfa12d6a73d65ab8f6df5513bf8a0b81022cc1
Mahesh <seleniumbymahesh@gmail.com>
2018-08-18 11:48:39 +0530

    sample1

--- project1/src/pack3/Sample1.java
+++ project1/src/pack3/Sample1.java
@@ -3,7 +3,8 @@ package pack3;
 public class Sample1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	System.out.println("HI");
+	System.out.println("Bye");
 
 	}
 
